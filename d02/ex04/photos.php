685fd0a3b5e9216c682ef9711811008f28f89182
Victoria Nguyen <vinguyen@e1z3r4p17.42.us.org>
2020-02-28 00:19:15 -0800

    Now done

--- d02/ex04/photos.php
+++ d02/ex04/photos.php
@@ -2,11 +2,12 @@
 <?php
 if ($argc == 2) {
     $html_contents = file_get_contents($argv[1]);
-    $regex = "/<img src=\"(https:\/\/[^\"]*)\"[^>]*>/";
+    $regex = "/<img src=\"https:\/\/([^\"]*)\"[^>]*>/";
     preg_match_all($regex, $html_contents, $matches);
     preg_match("/[^\/]*\//", $matches[1][0], $filename);
     $dir = $filename[0];
-    file_put_contents(basename($matches[1][0],"/"), fopen($matches[1][0] ,"r"));
-    rename(basename($matches[1][0],"/"), $dir . "/" . basename($matches[1][0],"/"));
+    file_put_contents(basename($matches[1][0],"/"), fopen("https://" . $matches[1][0] ,"r"));
+    mkdir($dir);
+    rename(basename($matches[1][0],"/"), $dir . basename($matches[1][0],"/"));
 }
 ?>
